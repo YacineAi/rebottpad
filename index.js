7a1e35484d75e788fc237c74c4aeabd344d11041
Yacine Djenidi <djenidi.dev@gmail.com>
2023-11-15 00:18:30 +0100

    Update index.js
    fix num navi

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ botly.on("message", async (senderId, message) => {
         }
       } else {
         if (message.message.text.length <= 3) {
-          if (message.message.text.length < 3 && !isNaN(message.message.text)) {
+          if (message.message.text.length <= 3 && !isNaN(message.message.text)) {
             if (user[0].pad != null) {
               var pi = parseInt(message.message.text);
               const read = await axios.get(`https://www.wattpad.com/api/v3/stories/${user[0].pad}?drafts=0&include_deleted=1&fields=id%2Ctitle%2Clength%2CcreateDate%2CmodifyDate%2CvoteCount%2CreadCount%2CcommentCount%2Curl%2Cpromoted%2Csponsor%2Clanguage%2Cuser%2Cdescription%2Ccover%2Chighlight_colour%2Ccompleted%2CisPaywalled%2CpaidModel%2Ccategories%2CnumParts%2CreadingPosition%2Cdeleted%2CdateAdded%2ClastPublishedPart%28createDate%29%2Ctags%2Ccopyright%2Crating%2Cstory_text_url%28text%29%2C%2Cparts%28id%2Ctitle%2CvoteCount%2CcommentCount%2CvideoId%2CreadCount%2CphotoUrl%2CmodifyDate%2CcreateDate%2Clength%2Cvoted%2Cdeleted%2Ctext_url%28text%29%2Cdedication%2Curl%2CwordCount%29%2CisAdExempt%2CtagRankings`, { headers : headers2});
